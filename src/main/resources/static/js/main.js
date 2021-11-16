6fa9dcdc8de45bc65d74b2a632d7fd3a7006860d
flumpyz <natalianata2001@mail.ru>
2021-11-16 21:09:48 +0500

    added the ability to mark a product as purchased

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,11 +2,16 @@ function writeItem() {
     let container = document.getElementById("container");
 
     let div = document.createElement("div");
+
+    let checkbox = document.createElement("input");
     let span = document.createElement("span");
     let button = document.createElement("button");
 
     let inputText = document.getElementById("input-item");
     span.innerText = inputText.value;
+
+    checkbox.type = "checkbox";
+    checkbox.setAttribute("onchange", "updateText(event)");
 
     button.className = "add-button";
     button.innerText = "×";
@@ -14,6 +19,7 @@ function writeItem() {
 
     div.className = "item-container";
 
+    div.append(checkbox);
     div.append(span);
     div.append(button);
 
@@ -24,4 +30,14 @@ function writeItem() {
 
 function removeItem(event) {
     event.currentTarget.parentNode.remove();
+}
+
+function updateText(event) {
+    let text = event.currentTarget.nextSibling;
+
+    if (text.style.textDecoration === "line-through") {
+        text.style.textDecoration = "none";
+    } else {
+        text.style.textDecoration = "line-through";
+    }
 }
